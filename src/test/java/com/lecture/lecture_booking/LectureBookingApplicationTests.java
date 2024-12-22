76e0e4af50282c51f474de875873488ee83c9630
kimy12 <shinkim7941@gmail.com>
2024-12-22 20:08:07 +0900

    Initial commit

---
+++ src/test/java/com/lecture/lecture_booking/LectureBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecture.lecture_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
