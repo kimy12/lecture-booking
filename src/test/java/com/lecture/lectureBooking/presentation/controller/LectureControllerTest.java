51c28776c5661d7ea8eb575ecb51ae4f147bf60c
kimy12 <shinkim7941@gmail.com>
2024-12-25 12:32:42 +0900

    feat : 날짜별

--- src/test/java/com/lecture/lectureBooking/presentation/controller/LectureControllerTest.java
+++ src/test/java/com/lecture/lectureBooking/presentation/controller/LectureControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lecture.lectureBooking.application.facade.LectureFacade;
 import com.lecture.lectureBooking.application.service.LectureService;
 import com.lecture.lectureBooking.domain.LectureBookingStatus;
+import com.lecture.lectureBooking.domain.LectureMembers;
 import com.lecture.lectureBooking.domain.Lectures;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -62,7 +63,7 @@ class LectureControllerTest {
 
     @DisplayName("날짜별로 현재 신청 가능한 특강 목록 조회 시, 목록이 존재하지 않은 경우 204 No Content를 반환한다.")
     @Test
-    void getNoAvailableLecturesByDate() throws Exception {
+    void nothingToGetAvailableLecturesByDate() throws Exception {
         // given
         String lectureAt = "20241224";
 
@@ -81,6 +82,63 @@ class LectureControllerTest {
                 .andExpect(jsonPath("$.code").value("204"))
                 .andExpect(jsonPath("$.data").isEmpty())
         ;
+    }
+
+    @DisplayName("특정 유저 아이디로 신청 완료된 특강 목록을 조회한다.")
+    @Test
+    void getBookedLecturesByUser() throws Exception {
+        // given
+        String lectureAt = "20241224";
+        LocalDateTime lectureStartAt = LocalDateTime.of(2024, 12, 24, 14, 0);
+
+        LectureMembers bookedLecture1 = getBookedLecture(1, lectureStartAt, createLecture("특강1", "강사1", lectureStartAt));
+        LectureMembers bookedLecture2 = getBookedLecture(1, lectureStartAt, createLecture("특강2", "강사2", lectureStartAt));
+
+        List<LectureMembers> lectureMembers = List.of(bookedLecture1, bookedLecture2);
+
+        Mockito.when(lectureFacade.showBookedLectures(1))
+                .thenReturn(lectureMembers);
+
+        // when // then
+        mockMvc.perform(
+                        get("/lecture/api/v1/bookedLectures")
+                                .param("userId", "1")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.data").isArray())
+        ;
+    }
+
+    @DisplayName("특정 유저 아이디로 신청 완료된 특강 목록을 조회 시, 목록이 존재하지 않는경우 204 No Content를 반환한다.")
+    @Test
+    void nothingToGetBookedLecturesByUser() throws Exception {
+        // given
+        long userId = 1;
+        Mockito.when(lectureFacade.showBookedLectures(userId))
+                .thenReturn(null);
+
+        // when // then
+        mockMvc.perform(
+                        get("/lecture/api/v1/bookedLectures")
+                                .param("userId", "1")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("NO_CONTENT"))
+                .andExpect(jsonPath("$.message").value("NO_CONTENT"))
+                .andExpect(jsonPath("$.code").value("204"))
+                .andExpect(jsonPath("$.data").isEmpty())
+        ;
+    }
+
+    private static LectureMembers getBookedLecture(long id,LocalDateTime lectureStartAt, Lectures lecture) {
+        return LectureMembers.builder()
+                .userId(id)
+                .lecture(lecture)
+                .createdAt(lectureStartAt)
+                .build();
     }
 
     private static Lectures createLecture(String title, String lecturer, LocalDateTime lectureAt) {
